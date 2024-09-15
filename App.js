6c16360b62cb51a59b00313c6d2621c82540e935
Vivek Yadav <yadavivek72@gmail.com>
2024-09-15 14:39:23 +0530

    before using Jsx chapter 3

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 /**
  * <div id='parent'>
  *      <div id='child'>
@@ -18,7 +20,7 @@
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     // To create sibling we can pass array of element
-    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h1", {}, "This is Namaste React"),
     React.createElement("h2", {}, "I am an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Namaste React
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR - Hot Module Replcement (Autorefresh on save)
+- File watching algorithim - written in C++ Library
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing (Read about it)
+- code splitting
+- Differential Bundeling (bundled for different browsers like IE, Edge, Firefox) - support older browser
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
