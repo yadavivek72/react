65c7c1ed08118adb6c51b9118a7aeba6ad5f92b8
Vivek Yadav <yadavivek72@gmail.com>
2024-09-17 22:17:47 +0530

    before laying foundation

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
+const Title = () => {
+  return <h1>Namastey React using JSX</h1>;
+};
+
+// React Functional Component
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Title />
+      <h1>Namaste React Functional Component</h1>
+    </div>
+  );
+};
+
+// React Element
+const heading = (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+    {HeadingComponent()}
+  </h1>
 );
 
-// jSX - HTML- like or XML - like syntax
-// JSX (transpiled or converted in to JS engine understandle code before it reaches the JS engine)
-// even your react.render doesn't understand jsx
-// parcel transpiled jsx to browser understandble code using babel
-
-const jsxHeading = <h1>Namaste React using JSX</h1>;
-
-console.log("jsx", jsxHeading);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// The job render function is to take this heading object which is react element and covert into browser understandable html h1 element and put it inside root
-root.render(heading);
+
+root.render(<HeadingComponent />);
